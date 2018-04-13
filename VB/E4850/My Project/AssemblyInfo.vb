1cca483be5fbf4cbe910e4d948a263eed8803776
DevExpressExampleBot <fakeEmail>
2018-04-13 04:41:08 +0300

    Source auto update [skip ci]

--- VB/E4850/My Project/AssemblyInfo.vb
+++ VB/E4850/My Project/AssemblyInfo.vb
@@ -1,5 +1,4 @@
-﻿Imports Microsoft.VisualBasic
-Imports System.Reflection
+﻿Imports System.Reflection
 Imports System.Resources
 Imports System.Runtime.CompilerServices
 Imports System.Runtime.InteropServices
@@ -8,11 +7,11 @@ Imports System.Windows
 ' General Information about an assembly is controlled through the following 
 ' set of attributes. Change these attribute values to modify the information
 ' associated with an assembly.
-<Assembly: AssemblyTitle("vb_E4850")>
+<Assembly: AssemblyTitle("E4850")>
 <Assembly: AssemblyDescription("")>
 <Assembly: AssemblyConfiguration("")>
 <Assembly: AssemblyCompany("")>
-<Assembly: AssemblyProduct("vb_E4850")>
+<Assembly: AssemblyProduct("E4850")>
 <Assembly: AssemblyCopyright("Copyright ©  2013")>
 <Assembly: AssemblyTrademark("")>
 <Assembly: AssemblyCulture("")>
@@ -32,11 +31,11 @@ Imports System.Windows
 '[assembly: NeutralResourcesLanguage("en-US", UltimateResourceFallbackLocation.Satellite)]
 
 
-	'(used if a resource is not found in the page, 
-	' or application resource dictionaries)
-	'(used if a resource is not found in the page, 
-	' app, or any theme specific resource dictionaries)
-<Assembly: ThemeInfo(ResourceDictionaryLocation.None, ResourceDictionaryLocation.SourceAssembly)>
+<Assembly: ThemeInfo(ResourceDictionaryLocation.None, ResourceDictionaryLocation.SourceAssembly)> 'where the generic resource dictionary is located - where theme specific resource dictionaries are located
+    '(used if a resource is not found in the page, 
+    ' or application resource dictionaries)
+    '(used if a resource is not found in the page, 
+    ' app, or any theme specific resource dictionaries)
 
 
 ' Version information for an assembly consists of the following four values:
